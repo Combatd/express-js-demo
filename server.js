c480719d2d04246b2de1cd92076d2b373a1cf159
Combatd <combatdstar@gmail.com>
2020-07-26 19:34:00 -0700

    Create eenvironment variables and Setup db.js in Server.js file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+
 const app = express();
-const PORT = 3000;
+const PORT =  process.env.PORT || 3000;
+require(',/config/db');
 
 const userRouter = require('./routes/users');
 
