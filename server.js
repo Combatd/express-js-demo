3fe884b9265f213d8e9a407ee66d10e808fc0103
Combatd <combatdstar@gmail.com>
2020-08-04 22:44:07 -0700

    Fix database import

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 const PORT =  process.env.PORT || 3000;
-require(',/config/db');
+require('./config/db');
 
 const userRouter = require('./routes/users');
 
